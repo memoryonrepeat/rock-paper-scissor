de0015599bc4c99f22e0fd77206abf344259fead
Thanh Nguyen <tienthanh8490@gmail.com>
2018-07-06 17:11:59 +0800

    test Player class

--- test/test.js
+++ test/test.js
@@ -1,8 +1,23 @@
-var assert = require('assert')
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1)
+const expect = require('chai').expect
+const HumanPlayer = require('../lib/player').HumanPlayer
+const ComputerPlayer = require('../lib/player').ComputerPlayer
+
+let human = new HumanPlayer('test_human')
+let computer1 = new ComputerPlayer()
+
+describe('Player', () => {
+  describe('#constructor()', () => {
+    it('human should be an object with class of HumanPlayer', () => {
+      expect(human.constructor.name).to.be.equal('HumanPlayer')
+    })
+    it('human should be an object with parent class of Player', () => {
+      expect(Object.getPrototypeOf(human.constructor).name).to.be.equal('Player')
+    })
+    it('computer1 should be an object with class of ComputerPlayer', () => {
+      expect(computer1.constructor.name).to.be.equal('ComputerPlayer')
+    })
+    it('computer1 should be an object with parent class of Player', () => {
+      expect(Object.getPrototypeOf(computer1.constructor).name).to.be.equal('Player')
     })
   })
 })
