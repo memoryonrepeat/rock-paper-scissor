572965e82cfb53bfdf1ac2706b04e8e625d9a31c
Thanh Nguyen <tienthanh8490@gmail.com>
2018-07-06 17:27:26 +0800

    handleInit() and use expect to test type

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 const expect = require('chai').expect
 const HumanPlayer = require('../lib/player').HumanPlayer
 const ComputerPlayer = require('../lib/player').ComputerPlayer
+const Player = require('../lib/player').Player
 
 let human = new HumanPlayer('test_human')
 let computer1 = new ComputerPlayer()
@@ -8,16 +9,28 @@ let computer1 = new ComputerPlayer()
 describe('Player', () => {
   describe('#constructor()', () => {
     it('human should be an object with class of HumanPlayer', () => {
-      expect(human.constructor.name).to.be.equal('HumanPlayer')
+      expect(human).to.be.an.instanceof(HumanPlayer)
     })
     it('human should be an object with parent class of Player', () => {
-      expect(Object.getPrototypeOf(human.constructor).name).to.be.equal('Player')
+      expect(Object.getPrototypeOf(human)).to.be.an.instanceof(Player)
     })
     it('computer1 should be an object with class of ComputerPlayer', () => {
-      expect(computer1.constructor.name).to.be.equal('ComputerPlayer')
+      expect(computer1).to.be.an.instanceof(ComputerPlayer)
     })
     it('computer1 should be an object with parent class of Player', () => {
-      expect(Object.getPrototypeOf(computer1.constructor).name).to.be.equal('Player')
+      expect(Object.getPrototypeOf(computer1)).to.be.an.instanceof(Player)
     })
+    it('object should has a name and initial score', () => {
+      expect(human.name).to.be.equal('test_human')
+      expect(human.score).to.be.equal(0)
+      expect(computer1.name).to.be.a('string')
+      expect(computer1.score).to.be.equal(0)
+    })
+  })
+})
+
+describe('Game', () => {
+  describe('#constructor()', () => {
+
   })
 })
